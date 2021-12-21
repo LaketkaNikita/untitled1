19a4db9e98adfae453e48a50e981ecdedc818466
LaketaNikita <laketkagilk@mail.ru>
2021-12-21 19:03:33 +0300

    new one

--- src/xD.java
+++ src/xD.java
@@ -1,11 +1,26 @@
 import java.util.ArrayList;
 import java.util.LinkedList;
 
-public class xD {
-    private static int D = 100000;
-    LinkedList<String> linked = new LinkedList<>();
-    ArrayList<String> array = new ArrayList<>();
+public class xD{
 
+
+    final static int D = 100000;
+    public LinkedList<String> link(){
+        LinkedList<String> link = new LinkedList<>();
+        for (int i = 0; i < D; i++) {
+            link.add("Da");
+        }
+        return link;
+    }
+
+
+    public ArrayList<String> array(){
+        ArrayList<String> link = new ArrayList<>();
+        for (int i = 0; i < D; i++) {
+            link.add("Da");
+        }
+        return link;
+    }
 
 
     public static void main(String[] args) {
@@ -19,81 +34,58 @@ public class xD {
     }
     //LL at the beginning
     public  long LinkedListAtTheBeginning() {
-        linked = new LinkedList<>();
-
-        for (int i = 0; i < D; i++) {
-            linked.add("Da");
-        }
-
         long startTime = System.nanoTime();
         for (int i = 0; i < D; i++) {
-            linked.add(0, "Da");
+            link().add(0, "Da");
         }
         return System.nanoTime() - startTime;
     }
 
     //AL at the beginning
     public long ArrayListAtTheBeginning() {
-        array = new ArrayList<>();
-        for (int i = 0; i < D; i++) {
-            array.add("Da");
-        }
+
         long startTime = System.nanoTime();
 
         for (int i = 0; i < D; i++) {
-            array.add(0, "Da");
+            array().add(0, "Da");
         }
         return System.nanoTime() - startTime;
     }
 
     //middle LL
     public long LinkedListAtTheMiddle() {
-        linked = new LinkedList<>();
-        for (int i = 0; i < D; i++) {
-            linked.add("Da");
-        }
         long startTime = System.nanoTime();
         for (int i = 0; i < D; i++) {
-            linked.add(50000, "Da");
+            link().add(50000, "Da");
         }
         return System.nanoTime() - startTime;
     }
 
     //middle AL
     public long ArrayListAtTheMiddle() {
-        array = new ArrayList<>();
-        for (int i = 0; i < D; i++) {
-            array.add("Da");
-        }
+
         long startTime = System.nanoTime();
         for (int i = 0; i < D; i++) {
-            array.add(50000, "Da");
+            array().add(50000, "Da");
         }
         return System.nanoTime() - startTime;
     }
 
     //end LL
     public long LinkedListAtTheEnd() {
-        linked = new LinkedList<>();
-        for (int i = 0; i < D; i++) {
-            linked.add("Da");
-        }
         long startTime = System.nanoTime();
         for (int i = 0; i < D; i++) {
-            linked.add("Da");
+            link().add("Da");
         }
         return System.nanoTime() - startTime;
     }
 
     //end AL
     public long ArrayListAtTheEnd() {
-        array = new ArrayList<>();
-        for (int i = 0; i < D; i++) {
-            array.add("Da");
-        }
+
         long startTime = System.nanoTime();
         for (int i = 0; i < D; i++) {
-            array.add("Da");
+            array().add("Da");
         }
         return System.nanoTime() - startTime;
     }
